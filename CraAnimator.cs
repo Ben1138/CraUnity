1817504bc841e93e206ddcd3237a4e82cf9b6f9d
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-04-15 22:36:06 +0200

    Init. Some code already written.

---
+++ CraAnimator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraAnimator : MonoBehaviour
+{
+    CraClip Clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity Cheap Runtime Animator
+<br/>
+A cheap and simple runtime Animator for skeletal meshes.<br/>
+Produce Animation clips, states and blending at runtime, NO Editor.<br/>
+Why? Because Unity still can't produce runtime AnimationClips that are NOT legacy (which is necessary to use Unity's Mecanim), although promised for over 6 years.
