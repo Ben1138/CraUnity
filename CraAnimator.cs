fd4a318e1a8a3aa2260903cc8a289625aeb16657
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-04-17 15:19:10 +0200

    Added CraPlayer. Assigning a Transform and playing a baked clip works. Removed Generic CraKey, support floats only.

--- CraAnimator.cs
+++ CraAnimator.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public class CraAnimator : MonoBehaviour
 {
-    CraClip Clip;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
-    }
+    
 }
