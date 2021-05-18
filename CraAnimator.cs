5bf3020d353d6c0267ecaacb33deb231adbc5870
Ben1138 <marten.mildt@web.de>
2021-05-18 17:26:57 +0200

    Shifted responibility of blending fully to CraPlayer. Another unsuccessfull attempt of animation blending. Introduced CraMonitor.

--- CraAnimator.cs
+++ CraAnimator.cs
@@ -3,53 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CraStateTransition
-{
-    public float Time = 1f;
-    public int[] Bones;
-}
-
-public struct CraState
-{
-    public CraPlayer Animation;
-    public CraStateTransition[] _Transitions;
-
-    public CraState(CraPlayer animation)
-    {
-        Animation = animation;
-        _Transitions = new CraStateTransition[CraSettings.MAX_STATES];
-    }
-}
-
-public struct CraPosRot
-{
-    public Vector3 Position;
-    public Quaternion Rotation;
-
-    public CraPosRot(Vector3 pos, Quaternion rot)
-    {
-        Position = pos;
-        Rotation = rot;
-    }
-}
 
 public class CraLayer
 {
-    public Dictionary<int, CraPosRot> BasePose;
-    public int CurrentStateIdx = CraSettings.STATE_NONE;
+    public int CurrentStateIdx { get; private set; } = CraSettings.STATE_NONE;
 
     public Action OnTransitFinished;
     public Action OnStateFinished;
 
-    public int BlendStateIdx = CraSettings.STATE_NONE;
-    public float BlendValue;
-
-    CraState[] States = new CraState[CraSettings.MAX_STATES];
+    CraPlayer[] States = new CraPlayer[CraSettings.MAX_STATES];
     bool[] StateSlots = new bool[CraSettings.MAX_STATES];
-    float TransitTimer;
     bool OnStateFinishedInvoked = false;
 
-    public int AddState(CraState state)
+    public int AddState(CraPlayer state)
     {
         if (!StateSlots[0])
         {
@@ -69,6 +35,15 @@ public class CraLayer
         return CraSettings.STATE_NONE;
     }
 
+    public CraPlayer GetCurrentState()
+    {
+        if (CurrentStateIdx != CraSettings.STATE_NONE)
+        {
+            return States[CurrentStateIdx];
+        }
+        return null;
+    }
+
     public bool RemoveState(int stateIdx)
     {
         Debug.Assert(stateIdx >= 0 && stateIdx < CraSettings.MAX_STATES);
@@ -84,174 +59,28 @@ public class CraLayer
         Debug.Assert(stateIdx < CraSettings.MAX_STATES);
         if (CurrentStateIdx != CraSettings.STATE_NONE)
         {
-            States[CurrentStateIdx].Animation.Reset();
+            States[CurrentStateIdx].Reset();
         }
         CurrentStateIdx = stateIdx;
         if (stateIdx != CraSettings.STATE_NONE)
         {
-            States[stateIdx].Animation.Play();
+            States[stateIdx].Play();
             OnStateFinishedInvoked = false;
         }
     }
 
     public void SetPlaybackSpeed(int stateIdx, float playbackSpeed)
     {
-        States[stateIdx].Animation.PlaybackSpeed = playbackSpeed;
-    }
-
-    public void ConnectStates(int stateIdx1, int stateIdx2)
-    {
-        Debug.Assert(stateIdx1 >= 0 && stateIdx1 < CraSettings.MAX_STATES && StateSlots[stateIdx1]);
-        Debug.Assert(stateIdx2 >= 0 && stateIdx2 < CraSettings.MAX_STATES && StateSlots[stateIdx2]);
-
-        ref CraState state1 = ref States[stateIdx1];
-        ref CraState state2 = ref States[stateIdx2];
-
-        HashSet<int> bones = new HashSet<int>();
-        bones.UnionWith(state1.Animation.AssignedIndices);
-        bones.UnionWith(state2.Animation.AssignedIndices);
-
-        CraStateTransition transition = new CraStateTransition();
-        transition.Bones = bones.ToArray();
-
-        state1._Transitions[stateIdx2] = transition;
-        state2._Transitions[stateIdx1] = transition;
-    }
-
-    public void DisconnectStates(int stateIdx1, int stateIdx2)
-    {
-        Debug.Assert(stateIdx1 >= 0 && stateIdx1 < CraSettings.MAX_STATES && StateSlots[stateIdx1]);
-        Debug.Assert(stateIdx2 >= 0 && stateIdx2 < CraSettings.MAX_STATES && StateSlots[stateIdx2]);
-
-        ref CraState state1 = ref States[stateIdx1];
-        ref CraState state2 = ref States[stateIdx2];
-
-        state1._Transitions[stateIdx2] = null;
-        state2._Transitions[stateIdx1] = null;
-    }
-
-    public void TransitTo(int stateIdx)
-    {
-        if (BlendStateIdx != CraSettings.STATE_NONE)
-        {
-            Debug.LogWarning("There's already a transit taking place!");
-            return;
-        }
-        Debug.Assert(stateIdx != CraSettings.STATE_NONE && stateIdx < CraSettings.MAX_STATES && StateSlots[stateIdx]);
-
-        ref CraState from = ref States[CurrentStateIdx];
-        ref CraState to = ref States[BlendStateIdx];
-
-        from.Animation.IsPlaying = false;
-        to.Animation.IsPlaying = true;
-        to.Animation.Playback = from.Animation.Playback;
-        CurrentStateIdx = stateIdx;
-
-        //if (from._Transitions[stateIdx] == null)
-        //{
-        //    Debug.LogError($"Cannot transit from '{CurrentStateIdx}' to '{stateIdx}', they are not connected!");
-        //    return;
-        //}
-
-        //BlendStateIdx = stateIdx;
-        //TransitTimer = 0f;
-
-        //// from now on, WE are controlling the animation
-        //from.Animation.IsPlaying = false;
-        //to.Animation.IsPlaying = false;
-        //to.Animation.Playback = 0f;
-    }
-
-    public void BlendTo(int stateIdx, float blend)
-    {
-        BlendStateIdx = stateIdx;
-        BlendValue = blend;
+        States[stateIdx].PlaybackSpeed = playbackSpeed;
     }
 
     public void Tick(float deltaTime)
     {
-        if (BlendStateIdx != CraSettings.STATE_NONE)
+        if (CurrentStateIdx > -1)
         {
-            ref CraState from = ref States[CurrentStateIdx];
-            ref CraState to = ref States[BlendStateIdx];
-
-            CraPlayer aFrom = from.Animation;
-            CraPlayer aTo = to.Animation;
-
-            aFrom.Playback += deltaTime;
-            aTo.Playback += deltaTime;
-
-            if (aFrom.Playback > aFrom.Duration)
-            {
-                aFrom.Playback = 0;
-            }
-            if (aTo.Playback > aTo.Duration)
-            {
-                aTo.Playback = 0;
-            }
-
-            int frameIdxFrom = Mathf.FloorToInt(aFrom.Playback * aFrom.Clip.Fps);
-            int frameIdxTo = Mathf.FloorToInt(aTo.Playback * aTo.Clip.Fps);
-
-            CraStateTransition transition = from._Transitions[BlendStateIdx];
-
-            if (false)
-            {
-                BlendValue = TransitTimer / transition.Time;
-            }
-
-            Vector3 fromPos, toPos;
-            Quaternion fromRot, toRot;
-
-            for (int j = 0; j < transition.Bones.Length; ++j)
-            {
-                int idx = transition.Bones[j];
-
-                if (aFrom.Clip.Bones[idx].Curve == null)
-                {
-                    fromPos = BasePose[idx].Position;
-                    fromRot = BasePose[idx].Rotation;
-                }
-                else
-                {
-                    fromPos = aFrom.Clip.Bones[idx].Curve.BakedPositions[frameIdxFrom];
-                    fromRot = aFrom.Clip.Bones[idx].Curve.BakedRotations[frameIdxFrom];
-                }
-
-                if (aTo.Clip.Bones[idx].Curve == null)
-                {
-                    toPos = BasePose[idx].Position;
-                    toRot = BasePose[idx].Rotation;
-                }
-                else
-                {
-                    toPos = aFrom.Clip.Bones[idx].Curve.BakedPositions[frameIdxTo];
-                    toRot = aFrom.Clip.Bones[idx].Curve.BakedRotations[frameIdxTo];
-                }
-
-                aFrom.AssignedBones[idx].localPosition = Vector3.Lerp(fromPos, toPos, BlendValue);
-                aFrom.AssignedBones[idx].localRotation = Quaternion.Slerp(fromRot, toRot, BlendValue);
-            }
-
-            if (false)
-            {
-                TransitTimer += deltaTime;
-                if (TransitTimer >= transition.Time)
-                {
-                    from.Animation.Reset();
-                    to.Animation.IsPlaying = true;
-                    CurrentStateIdx = BlendStateIdx;
-                    BlendStateIdx = CraSettings.STATE_NONE;
-                    OnTransitFinished?.Invoke();
-                    OnStateFinishedInvoked = false;
-                }
-            }
-        }
-        else if (CurrentStateIdx > -1)
-        {
-            ref CraState state = ref States[CurrentStateIdx];
-            state.Animation.Update(deltaTime);
-            if (!OnStateFinishedInvoked && state.Animation.Finished)
+            CraPlayer state = States[CurrentStateIdx];
+            state.Update(deltaTime);
+            if (!OnStateFinishedInvoked && state.Finished)
             {
                 OnStateFinished?.Invoke();
                 OnStateFinishedInvoked = true;
@@ -265,13 +94,17 @@ public class CraAnimator : MonoBehaviour
     public Action<int> OnTransitFinished;
     public Action<int> OnStateFinished;
 
-    public CraLayer[] Layers = new CraLayer[CraSettings.STATE_LEVEL_COUNT];
-    Dictionary<int, CraPosRot> BasePose = new Dictionary<int, CraPosRot>();
+    protected CraLayer[] Layers = new CraLayer[CraSettings.STATE_MAX_LAYERS];
 
 
-    public int AddState(int layer, CraState state)
+    public int AddState(int layer, CraPlayer state)
     {
         return Layers[layer].AddState(state);
+    }
+
+    public CraPlayer GetCurrentState(int layer)
+    {
+        return Layers[layer].GetCurrentState();
     }
 
     public bool RemoveState(int layer, int stateIdx)
@@ -289,29 +122,9 @@ public class CraAnimator : MonoBehaviour
         Layers[layer].SetPlaybackSpeed(stateIdx, playbackSpeed);
     }
 
-    public void ConnectStates(int layer, int stateIdx1, int stateIdx2)
-    {
-        Layers[layer].ConnectStates(stateIdx1, stateIdx2);
-    }
-
-    public void DisconnectStates(int layer, int stateIdx1, int stateIdx2)
-    {
-        Layers[layer].DisconnectStates(stateIdx1, stateIdx2);
-    }
-
-    public void TransitTo(int layer, int stateIdx)
-    {
-        Layers[layer].TransitTo(stateIdx);
-    }
-
-    public void BlendTo(int layer, int stateIdx, float blend)
-    {
-        Layers[layer].BlendTo(stateIdx, blend);
-    }
-
     public void Tick(float deltaTime)
     {
-        for (int layer = 0; layer < CraSettings.STATE_LEVEL_COUNT; ++layer)
+        for (int layer = 0; layer < CraSettings.STATE_MAX_LAYERS; ++layer)
         {
             Layers[layer].Tick(deltaTime);
         }
@@ -319,17 +132,7 @@ public class CraAnimator : MonoBehaviour
 
     void Awake()
     {
-        void Parse(Transform t)
-        {
-            BasePose.Add(CraSettings.BoneHashFunction(t.name), new CraPosRot(t.localPosition, t.localRotation));
-            for (int i = 0; i < t.childCount; ++i)
-            {
-                Parse(t.GetChild(i));
-            }
-        }
-        Parse(transform);
-
-        for (int layer = 0; layer < CraSettings.STATE_LEVEL_COUNT; ++layer)
+        for (int layer = 0; layer < CraSettings.STATE_MAX_LAYERS; ++layer)
         {
             int idx = layer;
             Layers[layer] = new CraLayer();
---
+++ Editor/CraMonitor.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+
+public class CraMonitor : EditorWindow
+{
+    int ViewLayer = 0;
+    string[] ViewLayerNames;
+    
+    [MenuItem("Cra/Runtime Monitor")]
+    public static void OpenLuaEditor()
+    {
+        CraMonitor window = GetWindow<CraMonitor>();
+        window.Show();
+    }
+
+    void Awake()
+    {
+        ViewLayerNames = new string[CraSettings.STATE_MAX_LAYERS];
+        for (int i = 0; i < ViewLayerNames.Length; ++i)
+        {
+            ViewLayerNames[i] = "Layer " + i;
+        }
+    }
+
+    void Update()
+    {
+        Repaint();
+    }
+
+    void OnGUI()
+    {
+        string memStr = "";
+        if (CraClip.GlobalBakeMemoryConsumption >= 1000)
+        {
+            memStr = (CraClip.GlobalBakeMemoryConsumption / 1000f).ToString() + " KB";
+        }
+        else if (CraClip.GlobalBakeMemoryConsumption >= 1000000)
+        {
+            memStr = (CraClip.GlobalBakeMemoryConsumption / 1000000f).ToString() + " MB";
+        }
+        else
+        {
+            memStr = CraClip.GlobalBakeMemoryConsumption.ToString() + " Bytes";
+        }
+        EditorGUILayout.LabelField("Animation Memory", memStr);
+        EditorGUILayout.Space();
+
+        if (Selection.activeGameObject == null)
+        {
+            EditorGUILayout.LabelField("Select a GameObject in Hierarchy!");
+            return;
+        }
+
+        CraAnimator anim = Selection.activeGameObject.GetComponent<CraAnimator>();
+        if (anim == null)
+        {
+            EditorGUILayout.LabelField("Selected GameObject does not have a CraAnimator component!");
+            return;
+        }
+
+        ViewLayer = EditorGUILayout.Popup(ViewLayer, ViewLayerNames);
+
+        CraPlayer state = anim.GetCurrentState(ViewLayer);
+        if (state != null)
+        {
+            EditorGUILayout.LabelField("Playback Speed");
+            state.PlaybackSpeed = EditorGUILayout.Slider(state.PlaybackSpeed, 0f, 10f);
+
+            if (state.SupportBlending)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Blending");
+                state.Blending = EditorGUILayout.Slider(state.Blending, 0f, 1f);
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Looping");
+            state.Looping = EditorGUILayout.Toggle(state.Looping);
+
+            EditorGUILayout.Space();
+            if (GUILayout.Button(state.IsPlaying ? "Stop" : "Play"))
+            {
+                if (state.IsPlaying)
+                {
+                    state.Reset();
+                }
+                else
+                {
+                    state.Play();
+                }
+            }
+
+            EditorGUILayout.Space();
+            float pos = EditorGUILayout.Slider(state.Playback, 0f, state.Duration);
+            if (pos != state.Playback)
+            {
+                state.Reset();
+                state.EvaluateFrame(pos);
+            }
+        }
+        else
+        {
+            EditorGUILayout.LabelField("NO STATE");
+        }
+    }
+}
