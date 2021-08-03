254f13ef18abc424bdcfef281e3f37c25c602b44
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-08-03 16:29:57 +0200

    Layer and Animator events now working again. Introduced CraMain as entry point. CraDataContainerManaged now returns refs on Get() (fixed a bug).

---
+++ CraMain.cs
@@ -0,0 +1,54 @@
+using System;
+
+
+public class CraMain
+{
+    CraPlaybackManager Players;
+    CraAnimatorManager Animators;
+
+    public CraMain()
+    {
+        Players = CraPlaybackManager.Get();
+        Animators = CraAnimatorManager.Get();
+    }
+
+    public void Tick()
+    {
+        Players.Tick();
+        Animators.Tick();
+    }
+
+    public void Destroy()
+    {
+        Players.Destroy();
+        Players = null;
+
+        Animators.Destroy();
+        Animators = null;
+    }
+}
+
+public class CraStatistics
+{
+    public CraMeasure PlayerData;
+    public CraMeasure ClipData;
+    public CraMeasure BakedClipTransforms;
+    public CraMeasure BoneData;
+    public CraMeasure Bones;
+}
+
+public static class CraSettings
+{
+    public const int STATE_NONE = -1;
+    public const int MAX_PLAYERS = 16384;
+    public const int MAX_LAYERS = 4096;
+    public const int MAX_ANIMATORS = 2048;
+
+    public const int MAX_PLAYER_DATA = MAX_PLAYERS / 4;
+    public const int MAX_CLIP_DATA = 256;
+    public const int MAX_BAKED_CLIP_TRANSFORMS = 65535 * 4;
+    public const int MAX_BONE_DATA = 65535 * 4;
+    public const int MAX_BONES = 65535 * 4;
+
+    public static Func<string, int> BoneHashFunction;
+}
--- CraMainComponent.cs
+++ CraMainComponent.cs
@@ -2,54 +2,25 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Add this component to your scene, or make an instance of CraMain yourself somewhere and call the 'Tick' and 'Destroy' methods accordingly.
+/// </summary>
 public class CraMainComponent : MonoBehaviour
 {
-    CraPlaybackManager Players;
-    CraAnimatorManager Animators;
+    CraMain Cra;
 
     void Awake()
     {
-        Players = CraPlaybackManager.Get();
-        Animators = CraAnimatorManager.Get();
+        Cra = new CraMain();
     }
 
     void Update()
     {
-        Players.Tick();
-        Animators.Tick();
+        Cra.Tick();
     }
 
     void OnDestroy()
     {
-        Players.Destroy();
-        Players = null;
-
-        Animators.Destroy();
-        Animators = null;
+        Cra.Destroy();
     }
-}
-
-public class CraStatistics
-{
-    public CraMeasure PlayerData;
-    public CraMeasure ClipData;
-    public CraMeasure BakedClipTransforms;
-    public CraMeasure BoneData;
-    public CraMeasure Bones;
-}
-
-public static class CraSettings
-{
-    public const int STATE_NONE = -1;
-    public const int MAX_PLAYERS = 16384;
-    public const int MAX_LAYERS = 4096;
-    public const int MAX_ANIMATORS = 2048;
-
-    public const int MAX_PLAYER_DATA = MAX_PLAYERS / 4;
-    public const int MAX_CLIP_DATA = 256;
-    public const int MAX_BAKED_CLIP_TRANSFORMS = 65535 * 4;
-    public const int MAX_BONE_DATA = 65535 * 4;
-    public const int MAX_BONES = 65535 * 4;
-
-    public static Func<string, int> BoneHashFunction;
 }
